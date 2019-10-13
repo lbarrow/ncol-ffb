d3c080362d618870933874f2674ee46018c6c196
luke <luke@ncol.com>
2019-10-13 08:45:26 -0700

    change parsing to every twominutes

--- utility/cronSetup.js
+++ utility/cronSetup.js
@@ -2,5 +2,7 @@ const cron = require('node-cron')
 const { getCurrentGameData } = require('../utility/dataManager')
 
 // cron.schedule('*/30 * * * * *', async () => {
-//   await getCurrentGameData()
-// })
+cron.schedule('*/2 * * * *', async () => {
+  // parse every two min
+  await getCurrentGameData()
+})
